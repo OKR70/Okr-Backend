fff294f8b63ab753d5db5429982c0d6ec35e3010
Ilya Gornikov <ilagornikov8@gmail.com>
2025-02-26 19:40:47 +0700

    Изменен vercel.json и проверка ролей

--- src/middlewares/hasRole.ts
+++ src/middlewares/hasRole.ts
@@ -3,19 +3,21 @@ import {
     Response,
     NextFunction
 } from 'express';
+import { Role } from '../consts/role';
 
 /*
  * Проверка наличия роли у пользователя
  */
 
-export const hasRole = (...roles: string[]) => {
+export const hasRole = (...roles: Role[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        for (const role of roles) {
-            if (req.user && role === req.user.role) {
-                return next();
+        if (req.user) {
+            for (const role of roles) {
+                if (req.user.role.includes(role)) {
+                    return next();
+                }
             }
         }
-
         return res.status(403).json({ error: 'Недостаточно прав' });
     };
 };
