6c86288443daf16d516a6c149872137096d0d994
Ilya Gornikov <ilagornikov8@gmail.com>
2025-03-01 12:16:40 +0700

    Исправлен проект

--- src/middlewares/uploadFile.ts
+++ src/middlewares/uploadFile.ts
@@ -1,10 +1,10 @@
-import FileUploadService from '../services/upload';
+// import FileUploadService from '../services/upload';
 
-export const uploadFile = async (req: any, res: any, next: any) => {
-    try {
-        FileUploadService.uploadFile(req, res, next);
-    } catch (error) {
-        console.error('Ошибка при загрузке файла:', error);
-        res.status(500).json({ message: 'Ошибка при загрузке файла' });
-    }
-};
+// export const uploadFile = async (req: any, res: any, next: any) => {
+//     try {
+//         FileUploadService.uploadFile(req, res, next);
+//     } catch (error) {
+//         console.error('Ошибка при загрузке файла:', error);
+//         res.status(500).json({ message: 'Ошибка при загрузке файла' });
+//     }
+// };
